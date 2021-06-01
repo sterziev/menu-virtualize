ff1d94d29c7218542726c4c542715b30ba26c946
stoil.terziev <stoil.terziev@ezbob.com>
2021-06-01 10:07:35 +0300

    fix name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/menu-virtulize'),
+      dir: require('path').join(__dirname, './coverage/menu-virtualize'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
